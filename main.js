4e9251107dd87490b5ecabb7c3e286a691035cf8
julieta.castano <julieta.castanosanchez@gmail.com>
2022-08-25 13:28:29 -0300

    Ãagregue fetch

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-const productos = [
-    {id: 1, nombre: "Vela", img: "./Imagenes/vela.jpg", precio: 2000},
-    {id: 2, nombre: "Set de velas", img: "./Imagenes/velas.jpg", precio: 7000},
-    {id: 3, nombre: "Set de te", img: "./Imagenes/sette.jpg", precio: 6000},
-    {id: 4, nombre: "Tetera", img: "./Imagenes/tetera.jpg", precio: 3000},
-    {id: 5, nombre: "Porta sahumerios", img: "./Imagenes/sahumerio.jpg", precio: 1000},
-    {id: 6, nombre: "Taza", img: "./Imagenes/taza.jpg", precio: 1500}
-];
-
 const idDeCarrito = `ItemDeCarrito`
 const carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
 let buttonCarrito = document.getElementById('button-carrito');
@@ -15,15 +6,18 @@ let precioTotal = carrito.reduce((acumulador, producto) => acumulador + producto
 console.log(precioTotal);
 
 let tablaCompras = document.getElementById('tablaCarrito');
-let cuerpoTabla = document.getElementById('cuerpoTabla')
+let cuerpoTabla = document.getElementById('cuerpoTabla');
+let cantidad = 0
 
+
+//refresh carrito -----------------------------------
 carrito.forEach(producto => {
     let itemCarrito = document.createElement('tr') 
     itemCarrito.innerHTML +=`
         <tr id=${idDeCarrito}>
             <td>${producto.nombre}</td>
             <td>$${producto.precio}</td>
-            <button type="button" class="btn btn-outline-danger align-middle"> X </button>
+            <button type="button" class="btn btn-outline-danger align-middle" onclick="eliminarItem(${producto.id})"    > X </button>
         </tr>`
     cuerpoTabla.appendChild(itemCarrito);
 
@@ -31,69 +25,103 @@ carrito.forEach(producto => {
     totalCompra.innerHTML = `<tr><th scope="col">Precio total: $${precioTotal}</th></tr>`
 });
 
-//Crear cards
-let cards = document.getElementById("cards");
-productos.forEach((producto) => {
-    const buttonId = `${producto.id}`;
-    let cards = document.getElementById("cards");
-    cards.innerHTML += `
-    <div class="card">
-        <img src="${producto.img}" class="card-img-top" alt="producto">
-        <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <h6 class="card-title">$${producto.precio}</h6>
-            <a class="btn btn-secondary" id=${buttonId}>Agregar al carrito</a>
-        </div>
-    </div>`
-});
-
-//Agregar al carrito
-productos.forEach((producto) => {
-    const buttonId = `${producto.id}`
-    document.getElementById(buttonId).addEventListener('click', () =>{
-        let indexProducto = productos.findIndex(producto => producto.id == buttonId);
-        let productoAgregado = productos[indexProducto];
-        carrito.push(productoAgregado);
-        console.log(carrito);
-        buttonCarrito.innerHTML = `<button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-cart-shopping"></i>  Items: ${carrito.length} </button>`
-        let precioTotal = carrito.reduce((acumulador, producto) => acumulador + producto.precio,0);
-        console.log(precioTotal);
-
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-
-        let itemCarrito = document.createElement('tr') 
-        itemCarrito.innerHTML +=`
-            <tr id=${idDeCarrito}>
-                <td>${producto.nombre}</td>
-                <td>$${producto.precio}</td>
-                <button type="button" class="btn btn-outline-danger align-middle"> X </button>
-            </tr>`
-        cuerpoTabla.appendChild(itemCarrito);
-
-        let totalCompra = document.getElementById('totalCompra');
-        totalCompra.innerHTML = `<tr><th scope="col">Precio total: $${precioTotal}</th></tr>`
-
-        //tostify
-        Toastify({
-            text: "Se agrego el producto al carrito!",
-            className: "info",
-            style: {background: "#e8c7bd",}
-        }).showToast();
-
-    })
-} )
-console.log(carrito)
-
-//vaciar carrito
-function vaciarCarrito(){
-    carrito.splice(0,carrito.length);
-    localStorage.clear();
-    buttonCarrito.innerHTML = `<button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-cart-shopping"></i>  Items: 0 </button>`
+const traerProductos = async() => {
+    const response = await fetch('productos.json');
+    const data = await response.json();
     
-    cuerpoTabla.innerHTML = ``
+    //Crear cards --------------------------------------------------------------------
+    let cards = document.getElementById("cards");
+    data.forEach((producto) => {
+        const buttonId = `${producto.id}`;
+        let cards = document.getElementById("cards");
+        cards.innerHTML += `
+        <div class="card">
+            <img src="${producto.img}" class="card-img-top" alt="producto">
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <h6 class="card-title">$${producto.precio}</h6>
+                <a class="btn btn-secondary" id=${buttonId}>Agregar al carrito</a>
+            </div>
+        </div>`
+    }); 
 
-    let totalCompra = document.getElementById('totalCompra');
-    totalCompra.innerHTML = `<tr><th scope="col">Precio total: $0 </th></tr>`;
+    //Agregar al carrito -----------------------------------------------------------
+    data.forEach((producto) => {
+        const buttonId = `${producto.id}`
+        document.getElementById(buttonId).addEventListener('click', () =>{
+            let indexProducto = data.findIndex(producto => producto.id == buttonId);
+            let productoAgregado = data[indexProducto];
+            carrito.push(productoAgregado);
+            console.log(carrito);
 
-    console.log(carrito);
+            buttonCarrito.innerHTML = `<button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-cart-shopping"></i>  Items: ${carrito.length} </button>`
+            let precioTotal = carrito.reduce((acumulador, producto) => acumulador + producto.precio,0);
+            console.log(precioTotal);
+
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+
+            let itemCarrito = document.createElement('tr') 
+            itemCarrito.innerHTML +=`
+                <tr id=${idDeCarrito}>
+                    <td>${producto.nombre}</td>
+                    <td>$${producto.precio}</td>
+                    <button type="button" class="btn btn-outline-danger align-middle" onclick="eliminarItem(${producto.id})"> X </button>
+                </tr>`
+            cuerpoTabla.appendChild(itemCarrito);
+
+            let totalCompra = document.getElementById('totalCompra');
+            totalCompra.innerHTML = `<tr><th scope="col">Precio total: $${precioTotal}</th></tr>`
+
+            //tostify --------------------------------------
+            Toastify({
+                text: "Se agrego el producto al carrito!",
+                className: "info",
+                style: {background: "#e8c7bd",}
+            }).showToast();
+
+        })
+    } )
+    console.log(carrito)
+}
+traerProductos()
+
+//vaciar carrito -----------------------------------------------------------
+function vaciarCarrito(){
+    Swal.fire({
+        title: 'Estas seguro que queres vaciar el carrito?',
+        icon: 'warning',    
+        showCancelButton: true,
+        confirmButtonText: 'si, vaciar',
+        cancelButtonText: 'No, cancelar!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            carrito.splice(0,carrito.length);
+            localStorage.clear();
+            buttonCarrito.innerHTML = `<button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-cart-shopping"></i>  Items: 0 </button>`
+            
+            cuerpoTabla.innerHTML = ``
+        
+            let totalCompra = document.getElementById('totalCompra');
+            totalCompra.innerHTML = `<tr><th scope="col">Precio total: $0 </th></tr>`;
+        
+            console.log(carrito);
+            location.reload();
+        } else if (result.isDenied) {
+            Swal.fire('Gracias', '', 'success')
+        }
+    })
+}
+
+//eliminar item -------------------------------------------------------------
+function eliminarItem(IdProducto){
+    let indexEliminar = carrito.findIndex(eliminar => eliminar.id == IdProducto);
+    console.log(indexEliminar)
+    
+    carrito.splice(indexEliminar,1);
+    console.log(carrito)
+    
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    
+    location.reload();
 }
